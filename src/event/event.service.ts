686d8913b262f51d2c4b2d3b389a6e202d7e7c8f
DevinduSamarasinghe <devindu990@gmail.com>
2024-09-24 13:55:00 +0530

    feat: ssl-certificates added

--- src/event/event.service.ts
+++ src/event/event.service.ts
@@ -1,4 +1,4 @@
-import { Injectable, NotFoundException, UnauthorizedException } from "@nestjs/common";
+import { Injectable, NotFoundException, UnauthorizedException, Logger } from "@nestjs/common";
 import { InjectModel } from "@nestjs/mongoose";
 import { Model } from "mongoose";
 import { Event } from "./schema/event.schema";
--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,40 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as fs from 'fs';
+import { ConfigService } from '@nestjs/config';
 import { ClerkAuthMiddleware } from './middleware';
 
 
 async function bootstrap() {
 
-  const app = await NestFactory.create(AppModule);
 
-  // Apply Clerk middleware globally
-  // app.use(new ClerkAuthMiddleware().use);
-  await app.listen(8080);
+  const configService = new ConfigService();
+
+  //https configurations
+  const httpsOptions = {
+    key: fs.readFileSync('./private.key'),
+    cert: fs.readFileSync('./server.crt')
+  };
+
+  //create app and other modules (loggers)
+  const app = await NestFactory.create(AppModule,{
+    logger: ['error', 'warn', 'log', 'debug', 'verbose'],
+    httpsOptions
+  });
+
+  //enabling CORS
+  app.enableCors({
+    origin: [
+      configService.get<string>('LOCAL_HOST'),
+      configService.get<string>('PROD_HOST'),
+    ],
+
+    methods: 'GET, HEAD, PUT, PATCH, POST, DELETE',
+    credentials: true, // for token to be sent
+  })
+
+  //port to listen to
+  await app.listen(configService.get<string>('PORT') || 8080);
   
 }
 bootstrap();
